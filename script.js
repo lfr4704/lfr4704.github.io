4a423d57ee2a8a8a0a2210da9a6e04e31db3cb67
Luis F. Robles <33875275+lfr4704@users.noreply.github.com>
2017-12-19 15:31:58 -0800

    My personal web developer page

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	    var di = $("h1.intro");
+        di.animate({height: '180px', opacity: '0.4'}, "slow");
+        di.animate({width: '500px', opacity: '0.8'}, "slow");
+        di.animate({height: '180px', opacity: '0.4'}, "slow");
+        di.animate({width: '500px', opacity: '0.8'}, "slow");
+
+	//$('#myDiv').animate({opacity: 'hide', height: 'hide'}, 'slow'); //
+});
+
+$("a[href='#3a']").on('shown.bs.tab', function(){
+    google.maps.event.trigger(map, 'resize');
+});
