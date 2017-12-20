bb441e66936454b66e612b9098d9fbcc023101fb
Luis F. Robles <33875275+lfr4704@users.noreply.github.com>
2017-12-20 02:04:47 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	
 	    var di = $("h1.intro");
         di.animate({height: '180px', opacity: '0.4'}, "slow");
-        di.animate({width: '500px', opacity: '0.8'}, "slow");
+        di.animate({width: '440px', opacity: '0.8'}, "slow");
         di.animate({height: '180px', opacity: '0.4'}, "slow");
-        di.animate({width: '500px', opacity: '0.8'}, "slow");
+        di.animate({width: '440px', opacity: '0.8'}, "slow");
 
 	//$('#myDiv').animate({opacity: 'hide', height: 'hide'}, 'slow'); //
 });
